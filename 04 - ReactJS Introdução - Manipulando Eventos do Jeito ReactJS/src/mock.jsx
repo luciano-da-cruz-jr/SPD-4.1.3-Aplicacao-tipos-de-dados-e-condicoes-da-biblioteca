61fcd8f3ec0055f438d38540b43c4ee548661e32
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-08-23 15:44:48 -0300

    ReactJS Introdução - Manipulando Eventos do Jeito ReactJS V1

---
+++ 04 - ReactJS Introdução - Manipulando Eventos do Jeito ReactJS/src/mock.jsx
@@ -0,0 +1,25 @@
+const mock =
+[
+  {
+    id: 1,
+    name: 'Bruno Carneiro',
+    skills: ['HTML', 'Node', 'CSS', 'Webpack']
+  },
+  {
+    id: 2,
+    name: 'Aline Carneiro',
+    skills: ['HTML', 'React Native', 'Go', 'JS']
+  },
+  {
+    id: 3,
+    name: 'Luciano da Cruz Junior',
+    skills: ['HTML', 'CSS', 'JS']
+  },
+  {
+    id: 4,
+    name: 'Dayana Rangel de Souza da Cruz',
+    skills: ['HTML', 'CSS']
+  } 
+]
+ 
+export default mock
