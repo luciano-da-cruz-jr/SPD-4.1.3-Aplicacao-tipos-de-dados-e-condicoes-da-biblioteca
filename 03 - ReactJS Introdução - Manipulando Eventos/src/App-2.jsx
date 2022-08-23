bc0f134d14dd793192fd4338786b8b6a48d8ad19
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-08-23 15:41:16 -0300

    ReactJS Introdução - Manipulando Eventos V1

---
+++ 03 - ReactJS Introdução - Manipulando Eventos/src/App-2.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+const listCustomer =
+[
+  {
+    id: 1,
+    name: 'Bruno Carneiro',
+    skills: ['HTML', 'Node', 'CSS', 'Webpack']
+  },
+  {
+    id: 2,
+    name: 'Aline Carneiro',
+    skills: ['HTML', 'React Native', 'Go', 'JS']
+  },
+  {
+    id: 3,
+    name: 'Luciano da Cruz Junior',
+    skills: ['HTML', 'CSS', 'JS']
+  },
+  {
+    id: 4,
+    name: 'Dayana Rangel de Souza da Cruz',
+    skills: ['HTML', 'CSS']
+  }
+]
+
+const App = () => 
+{
+  const handleClick = (e, id) =>
+  {
+    console.log('deletar cliente')
+    alert(`ID do Cliente: ${id}`)
+  }
+
+  const renderCustomers = (customer, index) =>
+  {
+    return (
+      <div key={`customer-${customer.id}`}>
+        <li>{customer.name} <button onClick={(e) => handleClick(e, customer.id)}>Deletar Cliente x</button></li>
+        {customer.skills.map(renderSkills)}
+      </div>
+    )
+  }
+  
+
+  const renderSkills = (skill, index) => 
+  {
+    return (
+      <div style={{ paddingLeft: '30px'}} key={`skill-${index}`}>
+        <li>{skill}</li>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+        <p>Digital Innovation One</p>
+        <p>Bem vindo a nossa aula =D.</p>
+        <div>
+          <ul>{listCustomer.map(renderCustomers)}</ul>
+        </div>
+    </div>
+  );
+};
+
+export default App;
